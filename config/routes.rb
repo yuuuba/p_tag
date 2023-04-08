865ac48903de2c3f5092c44a511ff77744681baa
yu <nsabu45@gmail.com>
2023-04-08 15:39:21 +0900

    コントローラ、モデル#1

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'articles/new'
+  get 'articles/show'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
